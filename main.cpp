4673414e90a1d87a251d3097570bb4b99ecccb3f
Ludvig Kratzert <ludvig.kratzert@gmail.com>
2019-03-13 14:31:41 +0100

    added sound

--- main.cpp
+++ main.cpp
@@ -22,16 +22,63 @@ using namespace glm;
 #include "midifile/MidiFile.h"
 using namespace smf;
 
+//MidiPlayer
+#include "MidiPlayer/minisdl_audio.h"
+#define TSF_IMPLEMENTATION
+#include "MidiPlayer/tsf.h"
+#define TML_IMPLEMENTATION
+#include "MidiPlayer/tml.h"
+
+
+/* ––– DECLARATIONS FOR MIDIPLAYER –– */
+// Holds the global instance pointer
+static tsf* g_TinySoundFont;
+// Holds global MIDI playback state
+static double g_Msec = -2700;               //current playback time
+static tml_message* g_MidiMessage;  //next message to be played
+static void AudioCallback(void* data, Uint8 *stream, int len);
+
+
 
 int main(void) {
     
+    /* –– PLAY MIDI */
+    tml_message* TinyMidiLoader = NULL;
+    // Define the desired audio output format we request
+    SDL_AudioSpec OutputAudioSpec;
+    OutputAudioSpec.freq = 44100;
+    OutputAudioSpec.format = AUDIO_F32;
+    OutputAudioSpec.channels = 2;
+    OutputAudioSpec.samples = 4096;
+    OutputAudioSpec.callback = AudioCallback;
+    // Initialize the audio system
+    SDL_AudioInit(TSF_NULL);
+    // Load MIDI
+    TinyMidiLoader = tml_load_filename("MusicLibrary/impromptu.mid");
+    //Set up the global MidiMessage pointer to the first MIDI message
+    g_MidiMessage = TinyMidiLoader;
+    // Load the SoundFont from a file
+    g_TinySoundFont = tsf_load_filename("MusicLibrary/kawai.sf2");
+    // Set the SoundFont rendering output mode
+    tsf_set_output(g_TinySoundFont, TSF_STEREO_INTERLEAVED, OutputAudioSpec.freq, 0.0f);
+    // Request the desired audio output format
+    SDL_OpenAudio(&OutputAudioSpec, TSF_NULL);
+    // Start the actual audio playback here
+    // The audio thread will begin to call our AudioCallback function
+    SDL_PauseAudio(0);
+    //Wait until the entire MIDI file has been played back (until the end of the linked message list is reached)
+    //while (g_MidiMessage != NULL) SDL_Delay(100);
+    //tsf_close(g_TinySoundFont);
+    //tml_free(TinyMidiLoader);
+    /* –– */
+    
     MidiFile midifile;
-    midifile.read("MusicLibrary/twinkle.mid");
+    midifile.read("MusicLibrary/impromptu.mid");
     midifile.doTimeAnalysis();
     midifile.linkNotePairs();
     
     int tpq = midifile.getTicksPerQuarterNote();
-    int bpm = 100;
+    int bpm = 168;
     
     struct MIDI_note {
         int start; // time
@@ -319,6 +366,8 @@ int main(void) {
         glfwSwapBuffers(window);
         // Poll for and process events
         glfwPollEvents();
+        
+        SDL_Delay(30); //Play sound
     }
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window));
     
@@ -329,4 +378,41 @@ int main(void) {
     
     glfwTerminate();
     return 0;
+}
+
+static void AudioCallback(void* data, Uint8 *stream, int len)
+{
+    //Number of samples to process
+    int SampleBlock, SampleCount = (len / (2 * sizeof(float))); //2 output channels
+    for (SampleBlock = TSF_RENDER_EFFECTSAMPLEBLOCK; SampleCount; SampleCount -= SampleBlock, stream += (SampleBlock * (2 * sizeof(float))))
+    {
+        //We progress the MIDI playback and then process TSF_RENDER_EFFECTSAMPLEBLOCK samples at once
+        if (SampleBlock > SampleCount) SampleBlock = SampleCount;
+        
+        //Loop through all MIDI messages which need to be played up until the current playback time
+        for (g_Msec += SampleBlock * (1000.0 / 44100.0); g_MidiMessage && g_Msec >= g_MidiMessage->time; g_MidiMessage = g_MidiMessage->next)
+        {
+            switch (g_MidiMessage->type)
+            {
+                case TML_PROGRAM_CHANGE: //channel program (preset) change (special handling for 10th MIDI channel with drums)
+                    tsf_channel_set_presetnumber(g_TinySoundFont, g_MidiMessage->channel, g_MidiMessage->program, (g_MidiMessage->channel == 9));
+                    break;
+                case TML_NOTE_ON: //play a note
+                    tsf_channel_note_on(g_TinySoundFont, g_MidiMessage->channel, g_MidiMessage->key, g_MidiMessage->velocity / 127.0f);
+                    break;
+                case TML_NOTE_OFF: //stop a note
+                    tsf_channel_note_off(g_TinySoundFont, g_MidiMessage->channel, g_MidiMessage->key);
+                    break;
+                case TML_PITCH_BEND: //pitch wheel modification
+                    tsf_channel_set_pitchwheel(g_TinySoundFont, g_MidiMessage->channel, g_MidiMessage->pitch_bend);
+                    break;
+                case TML_CONTROL_CHANGE: //MIDI controller messages
+                    tsf_channel_midi_control(g_TinySoundFont, g_MidiMessage->channel, g_MidiMessage->control, g_MidiMessage->control_value);
+                    break;
+            }
+        }
+        
+        // Render the block of audio samples in float format
+        tsf_render_float(g_TinySoundFont, (float*)stream, SampleBlock, 0);
+    }
 }
